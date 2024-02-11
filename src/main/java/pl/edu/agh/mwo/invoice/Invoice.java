c74320262165b27434a56272a9cc131119a6289e
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2024-02-11 12:27:50 +0100

    Refactor code in order to cover tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -6,15 +6,12 @@ import java.util.*;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Set<Product> products = new HashSet<>();
-
-    private Map<String, Integer> qtys = new HashMap<String, Integer>();
+    private Map<Product, Integer> products = new HashMap<>();
     public void addProduct(Product product) throws IllegalArgumentException {
         if (product == null) {
             throw new IllegalArgumentException();
         }
-        products.add(product);
-        qtys.put(product.getName(), 1);
+        products.put(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) throws IllegalArgumentException {
@@ -26,16 +23,15 @@ public class Invoice {
             throw new IllegalArgumentException("Please add positive qty");
         }
 
-        products.add(product);
-        qtys.put(product.getName(), quantity);
+        products.put(product, quantity);
     }
 
     public BigDecimal getSubtotal() {
         BigDecimal subtotal = new BigDecimal(0);
 
-        for (Product product : products) {
-            for (int i = 0; i < qtys.get(product.getName()); i++) {
-                subtotal =  subtotal.add(product.getPrice());
+        for (Map.Entry<Product, Integer> set : products.entrySet()) {
+            for (int i = 0; i < set.getValue(); i++) {
+                subtotal =  subtotal.add(set.getKey().getPrice());
             }
         }
 
@@ -45,8 +41,9 @@ public class Invoice {
     public BigDecimal getTax() {
         BigDecimal tax = new BigDecimal(0);
 
-        for (Product product : products) {
-            for (int i = 0; i < qtys.get(product.getName()); i++) {
+        for (Map.Entry<Product, Integer> set : products.entrySet()) {
+            for (int i = 0; i < set.getValue(); i++) {
+                Product product = set.getKey();
                 tax =  tax.add(product.getPrice().multiply(product.getTaxPercent()));
             }
         }
